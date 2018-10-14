80cf6d19e504416a13ae043239f3f9609076d71f
sethidium <44013378+sethidium@users.noreply.github.com>
2018-10-14 12:08:19 +1100

    Navigation setup

--- resources/js/Scripts of DOOM.js
+++ resources/js/Scripts of DOOM.js
@@ -1,1 +1,21 @@
 cssVars();
+
+// open navigation
+
+$('.nav-prompt').click(function() {
+
+   if ($('.nav-inner').attr('aria-expanded') == 'false') {
+       $('.nav-inner').attr('aria-expanded', 'true');
+   }
+   else{
+    $('.nav-inner').attr('aria-expanded', 'false');
+   }
+//Change the Text//
+if ($('.nav-prompt').text() == 'Open Navigation') {
+   
+    $('.nav-prompt').text('Close Navigation'); }
+else {
+    $('.nav-prompt').text('Open Navigation');
+}
+})
+
